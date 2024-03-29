efa296185162a2a0293bb1084e756f0fac7a62a2
HuyDirvu <honhathuy098@gmail.com>
2024-03-29 10:57:14 +0700

    update xml

---
+++ app/src/main/java/com/example/todo/views/NewActivity.java
@@ -0,0 +1,16 @@
+package com.example.todo.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.todo.R;
+
+public class NewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new);
+    }
+}
